572095adf0f73bebbc6a236628330e2f95afa1bf
collinsctk <collinsctk@qytang.com>
2021-08-19 21:33:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ node = Flask(__name__)
 # 静态路由,最简单页面
 @node.route('/', methods=['GET'])
 def index():
-    return f"This is QYTAKS:{socket.gethostname()}, My IP is {socket.gethostbyname(socket.gethostname())}"
+    return f"This is WEBAPP:{socket.gethostname()}, My IP is {socket.gethostbyname(socket.gethostname())}"
 
 
 if __name__ == "__main__":
