feb8c8619b34257e5ca0838c9428843ca5d27723
collinsctk <collinsctk@qytang.com>
2021-08-20 08:44:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ node = Flask(__name__)
 # 静态路由,最简单页面
 @node.route('/', methods=['GET'])
 def index():
-    return f"This is WEBAPP DevOps Pipeline:{socket.gethostname()}, My IP is {socket.gethostbyname(socket.gethostname())}"
+    return f"This is WEBAPP DevOps:{socket.gethostname()}, My IP is {socket.gethostbyname(socket.gethostname())}"
 
 
 if __name__ == "__main__":
