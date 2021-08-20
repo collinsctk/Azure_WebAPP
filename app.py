d55276e9263190f1ae5735fa99086af0dda1bb62
collinsctk <collinsctk@qytang.com>
2021-08-20 09:37:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ node = Flask(__name__)
 # 静态路由,最简单页面
 @node.route('/', methods=['GET'])
 def index():
-    return f"Test Tag1:{socket.gethostname()}, My IP is {socket.gethostbyname(socket.gethostname())}"
+    return f"Test Tag2:{socket.gethostname()}, My IP is {socket.gethostbyname(socket.gethostname())}"
 
 
 if __name__ == "__main__":
